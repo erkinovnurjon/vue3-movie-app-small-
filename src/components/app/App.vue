9ba1e00db317d02150b0678ee8f34a5d9a6c971f
Nurjon <nurjon2477@gmail.com>
2023-11-17 23:48:36 +0500

    add pagination

--- src/components/app/App.vue
+++ src/components/app/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="app font-monospace">
     <div class="content">
-      <AppInfo :allMoviesCount="movies.length" :favouriteMoviesCount="movies.filter((c) => c.favourite).length" />
+      <AppInfo
+        :allMoviesCount="movies.length"
+        :favouriteMoviesCount="movies.filter((c) => c.favourite).length"
+      />
       <div class="search-panel">
         <SearchPanel :upDateTermHandler="upDateTermHandler" />
-        <AppFilter  :updateFilterHandler="updateFilterHandler" :filterName="filter" />
-        
+        <AppFilter
+          :updateFilterHandler="updateFilterHandler"
+          :filterName="filter"
+        />
       </div>
       <Box v-if="!movies.length && !isLoading">
         <p class="text-center fs-3 text-danger">Kinolar yo'q</p>
@@ -15,13 +20,27 @@
       </Box>
 
       <MovieListForm
-       v-else  
-      :movies="onFilterHandler(onSearchHandler(movies , term) , filter)"
+        v-else
+        :movies="onFilterHandler(
+          onSearchHandler(movies, term),
+          filter
+        )"
         @onToggle="onToggleHandler"
         @onRemove="onRemoveHandler"
       />
-      <Box>
-          <Pagination />
+      <Box class="d-flex justify-content-center">
+        <nav aria-label="pagination">
+          <ul class="pagination pagination-sm">
+            <li
+              v-for="pageNumber in totalPages"
+              :key="pageNumber"
+              @click="changePage(pageNumber)"
+              :class="{ active: pageNumber === page +1 }"
+            >
+              <span class="page-link">{{ pageNumber }}</span>
+            </li>
+          </ul>
+        </nav>
       </Box>
       <MovieAddForm @createMovie="createMovie" />
     </div>
@@ -34,9 +53,8 @@ import AppFilter from "../app-filter/AppFilter.vue";
 import SearchPanel from "../search-panel/SearchPanel.vue";
 import MovieAddForm from "../movie-add-form/MovieAddForm.vue";
 import MovieListForm from "../movie-list-form/MovieListForm.vue";
-import Loader from "@/ui-component/Loader.vue"
+import Loader from "@/ui-component/Loader.vue";
 import axios from "axios";
-import Pagination from "../../ui-component/Pagination.vue"
 import Box from "@/ui-component/Box.vue";
 
 export default {
@@ -48,17 +66,16 @@ export default {
     MovieAddForm,
     Box,
     Loader,
-    Pagination ,
-},
+  },
   data() {
     return {
       movies: [],
       term: "",
-      filter: 'all',
-      isLoading : false ,
-      limit : 10 ,
-      page : 0 ,
-      totalPages : 0 ,
+      filter: "all",
+      isLoading: false,
+      limit: 10,
+      page: 0,
+      totalPages: 0,
     };
   },
   methods: {
@@ -76,61 +93,71 @@ export default {
     onRemoveHandler(id) {
       this.movies = this.movies.filter((c) => c.id !== id);
     },
-    upDateTermHandler(term){
-      this.term = term
+    upDateTermHandler(term) {
+      this.term = term;
     },
     onSearchHandler(arr, term) {
       if (term.length === 0) {
         return arr;
       }
-      return arr.filter((c) => c.name.toLowerCase().indexOf(term.toLowerCase()) > -1);
+      return arr.filter(
+        (c) => c.name.toLowerCase().indexOf(term.toLowerCase()) > -1
+      );
     },
     onFilterHandler(arr, filter) {
       switch (filter) {
-        case 'popular':
-          return arr.filter(c => c.like)
-        case 'mostViewers':
-          return arr.filter(c => c.viewers > 30)
+        case "popular":
+          return arr.filter((c) => c.like);
+        case "mostViewers":
+          return arr.filter((c) => c.viewers > 30);
         default:
-          return arr
+          return arr;
       }
     },
-    updateFilterHandler(filter){
-      this.filter = filter
+    updateFilterHandler(filter) {
+      this.filter = filter;
+      this.page = 0; // Filtrlashda sahifani nolga sozlash
+      this.fetchMovie(); // Filtrlashda ma'lumotlarni qayta yuklash
     },
-    async fetchMovie (){
+    async fetchMovie() {
       try {
-        this.isLoading = true
-        setTimeout( async() => {
-          
-          const response = await axios.get('https://jsonplaceholder.typicode.com/posts',{
-            params:{
-              _limit : this.limit,
-              _page : this.page
-            }
-          });
-          const newArr = response.data.map(item => ({
-            id : item.id,
-            name:item.title ,
-            like : false ,
-            favourite : false ,
-            viewers : item.id * 10,
-          }));
-          this.totalPages = Math.ceil(response.headers['x-toral-count'] / this.limit)
-          this.movies = newArr;
-          this.isLoading = false
-        }, 300);
+        this.isLoading = true;
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts",
+          {
+            params: {
+              _limit: this.limit,
+              _page: this.page + 1, // Sahifa soni 1 dan boshlanadi
+            },
+          }
+        );
+        const newArr = response.data.map((item) => ({
+          id: item.id,
+          name: item.title,
+          like: false,
+          favourite: false,
+          viewers: item.id * 10,
+        }));
+        this.totalPages = Math.ceil(
+          response.headers["x-total-count"] / this.limit
+        );
+        this.movies = newArr;
+        this.isLoading = false;
       } catch (error) {
-        alert(error.message);
+        console.error(error.message);
+        this.isLoading = false;
       }
-    }
+    },
+    changePage(pageNumber) {
+      this.page = pageNumber - 1; // Sahifani o'zgartirish
+      this.fetchMovie(); // Ma'lumotlarni yangilash
+    },
   },
   mounted() {
     this.fetchMovie();
-  }
+  },
 };
 </script>
-
 
 <style>
 .app {
